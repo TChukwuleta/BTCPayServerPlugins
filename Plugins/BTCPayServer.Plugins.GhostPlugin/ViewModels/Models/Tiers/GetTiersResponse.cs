5a6d5d02ea99b3f9179ad44cfcbb5bc183ef7591
Chukwuleta Tobechi <47084273+TChukwuleta@users.noreply.github.com>
2025-02-28 13:35:52 +0100

    handle null conversion

--- Plugins/BTCPayServer.Plugins.GhostPlugin/ViewModels/Models/Tiers/GetTiersResponse.cs
+++ Plugins/BTCPayServer.Plugins.GhostPlugin/ViewModels/Models/Tiers/GetTiersResponse.cs
@@ -18,8 +18,8 @@ public class Tier
     public bool active { get; set; }
     public string type { get; set; }
     public object welcome_page_url { get; set; }
-    public DateTime created_at { get; set; }
-    public DateTime updated_at { get; set; }
+    public DateTime? created_at { get; set; }
+    public object updated_at { get; set; }
     public string visibility { get; set; }
     public List<object> benefits { get; set; }
     public int trial_days { get; set; }
