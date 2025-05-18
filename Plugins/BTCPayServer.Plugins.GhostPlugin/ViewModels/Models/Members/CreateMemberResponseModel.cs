37c9ec1b8e0f815dfa042d0868c91c5ca8ef9989
Chukwuleta Tobechi <47084273+TChukwuleta@users.noreply.github.com>
2025-05-18 13:05:53 +0100

    handle ghost membership error

--- Plugins/BTCPayServer.Plugins.GhostPlugin/ViewModels/Models/Members/CreateMemberResponseModel.cs
+++ Plugins/BTCPayServer.Plugins.GhostPlugin/ViewModels/Models/Members/CreateMemberResponseModel.cs
@@ -1,36 +1,19 @@
-﻿using System;
-using System.Collections.Generic;
-using Microsoft.AspNet.SignalR.Client.Hubs;
+﻿using System.Collections.Generic;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
 
 namespace BTCPayServer.Plugins.GhostPlugin.ViewModels.Models;
 
 public class CreateMemberResponseModel
 {
-    public List<MemberCreationResponse> members { get; set; }
+    public List<MemberCreationResponse> members { get; set; } = new List<MemberCreationResponse>();
 }
 public class MemberCreationResponse
 {
     public string id { get; set; }
     public string uuid { get; set; }
-    public string email { get; set; }
-    public string name { get; set; }
-    public object note { get; set; }
-    public object geolocation { get; set; }
-    public bool subscribed { get; set; }
-    public object created_at { get; set; }
-    public object updated_at { get; set; }
-    public List<object> labels { get; set; }
-    public List<object> subscriptions { get; set; }
-    public string avatar_image { get; set; }
-    public bool comped { get; set; }
-    public int email_count { get; set; }
-    public int email_opened_count { get; set; }
-    public object email_open_rate { get; set; }
-    public string status { get; set; }
-    public object last_seen_at { get; set; }
-    public object attribution { get; set; }
     public string unsubscribe_url { get; set; }
-    public List<object> tiers { get; set; }
-    public object email_suppression { get; set; }
-    public List<object> newsletters { get; set; }
+
+    [JsonExtensionData]
+    public Dictionary<string, JToken> AdditionalData { get; set; }
 }
