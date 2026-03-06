9710dad6d86e4fa4e557092058653c4cbc97d3de
Chukwuleta Tobechi <47084273+TChukwuleta@users.noreply.github.com>
2026-03-06 17:58:48 +0100

    bump jumpseller

--- Plugins/BTCPayServer.Plugins.JumpSeller/ViewModels/JumpSellerSettingsViewModel.cs
+++ Plugins/BTCPayServer.Plugins.JumpSeller/ViewModels/JumpSellerSettingsViewModel.cs
@@ -5,11 +5,11 @@ namespace BTCPayServer.Plugins.JumpSeller.ViewModels;
 public class JumpSellerSettingsViewModel
 {
     [Required]
-    [Display(Name = "EPG Account ID")]
+    [Display(Name = "Payment Method Key")]
     public string EpgAccountId { get; set; }
 
     [Required]
-    [Display(Name = "EPG Secret")]
+    [Display(Name = "Payment Method Secret")]
     public string EpgSecret { get; set; }
     public string StoreId { get; set; }
     public string PaymentUrl { get; set; }
