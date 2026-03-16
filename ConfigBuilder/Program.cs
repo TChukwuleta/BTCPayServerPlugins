f97068c33a76dfd65761cde33fbdcd3aa79c5347
Chukwuleta Tobechi <47084273+TChukwuleta@users.noreply.github.com>
2026-03-16 08:12:46 +0100

    debump btcpay

--- ConfigBuilder/Program.cs
+++ ConfigBuilder/Program.cs
@@ -1,15 +1,18 @@
 ﻿using System.Reflection;
 using System.Text.Json;
 
-var excluded = new HashSet<string> { "BTCPayServer.Plugins.ShopifyPlugin", "BTCPayServer.Plugins.Salesforce", "BTCPayServer.Plugins.SquareUp" };
-var plugins = Directory.GetDirectories("../../../../Plugins").Where(p => !excluded.Contains(Path.GetFileName(p)));
+/*var excluded = new HashSet<string> { "BTCPayServer.Plugins.ShopifyPlugin", "BTCPayServer.Plugins.Salesforce", "BTCPayServer.Plugins.SquareUp" };
+var plugins = Directory.GetDirectories("../../../../Plugins").Where(p => !excluded.Contains(Path.GetFileName(p)));*/
+
+var included = new HashSet<string> { "BTCPayServer.Plugins.SatoshiTickets" };
+var plugins = Directory.GetDirectories("../../../../Plugins").Where(p => included.Contains(Path.GetFileName(p)));
 var p = "";
 foreach (var plugin in plugins)
 {
     var assemblyConfigurationAttribute = typeof(Program).Assembly.GetCustomAttribute<AssemblyConfigurationAttribute>();
     var buildConfigurationName = assemblyConfigurationAttribute?.Configuration;
 
-    p += $"{Path.GetFullPath(plugin)}/bin/{buildConfigurationName}/net10.0/{Path.GetFileName(plugin)}.dll;";
+    p += $"{Path.GetFullPath(plugin)}/bin/{buildConfigurationName}/net8.0/{Path.GetFileName(plugin)}.dll;";
 }
 
 var content = JsonSerializer.Serialize(new
