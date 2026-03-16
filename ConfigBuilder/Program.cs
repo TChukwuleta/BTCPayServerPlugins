66f8a819ee39e0a4745be5a262182959c355722f
Chukwuleta Tobechi <47084273+TChukwuleta@users.noreply.github.com>
2026-03-16 08:34:33 +0100

    revert jumpseller

--- ConfigBuilder/Program.cs
+++ ConfigBuilder/Program.cs
@@ -4,7 +4,7 @@ using System.Text.Json;
 /*var excluded = new HashSet<string> { "BTCPayServer.Plugins.ShopifyPlugin", "BTCPayServer.Plugins.Salesforce", "BTCPayServer.Plugins.SquareUp" };
 var plugins = Directory.GetDirectories("../../../../Plugins").Where(p => !excluded.Contains(Path.GetFileName(p)));*/
 
-var included = new HashSet<string> { "BTCPayServer.Plugins.SatoshiTickets" };
+var included = new HashSet<string> { "BTCPayServer.Plugins.SatoshiTickets", "BTCPayServer.Plugins.JumpSeller" };
 var plugins = Directory.GetDirectories("../../../../Plugins").Where(p => included.Contains(Path.GetFileName(p)));
 var p = "";
 foreach (var plugin in plugins)
